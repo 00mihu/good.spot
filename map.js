8b1baec7f2271ba2e2e44820fb08824036280d7c
mihumihu <103144701+00mihu@users.noreply.github.com>
2023-08-03 09:26:02 +0200

    Add files via upload

---
+++ map.js
@@ -0,0 +1,63 @@
+var platform = new H.service.Platform({
+    'apikey': 'xSRg7dV_QxWVD-UCVrPh1RavZqMHqW7bdGwfPj19sWA'
+  });
+
+  // Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 13.5,
+      center: { lat: 55.67, lng: 12.59 }
+    });
+
+     // Create the default UI:
+ const ui = H.ui.UI.createDefault(map, defaultLayers);
+
+ // Enable the event system on the map instance:
+var mapEvents = new H.mapevents.MapEvents(map);
+
+
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position);
+
+  
+let browserPosition = {lat:position.coords.latitiude, lng:position.coords.longitude};
+let marker = new H.map.Marker(browserPosition, {icon:pos});
+map.addObject(marker);
+}
+); 
+} else {
+  alert("Geolocation not supported");
+}
+
+
+// Add event listeners:
+map.addEventListener('tap', function(evt) {
+  console.log(evt)
+  console.log(evt.type, evt.currentPointer.type);
+  if(evt.target instanceof H.map.Marker){
+    var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
+      content: evt.target.getData()
+    });
+    ui.addBubble(bubble);
+
+   
+}
+    // Log 'tap' and 'mouse' events:
+    else {
+
+    let pointer = evt.currentPointer;
+    let pointerPosition = map.screenToGeo(pointer.viewportX, pointer.viewportY);
+    let pointerMarker = new H.map.Marker(pointerPosition);
+    pointerMarker.setData('That is a good spot!');
+    map.addObject(pointerMarker);
+}
+});
+
+// Instantiate the default behavior, providing the mapEvents object:
+var behavior = new H.mapevents.Behavior(mapEvents);
